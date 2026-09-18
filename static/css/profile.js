6dd15f9c748be98152e38a6553499f51f699c55e
Sushmita Marasini <sushmita22comp2024@kcc.edu.np>
2026-09-18 21:03:50 +0545

    Improve profile picture controls

--- static/css/profile.js
+++ static/css/profile.js
@@ -1,6 +1,7 @@
 const storedPhoto = localStorage.getItem('mero-cycle-profile-photo');
 const photoPreview = document.querySelector('#profile-photo-preview');
 const photoInput = document.querySelector('#profile-photo');
+const removePhotoButton = document.querySelector('#remove-profile-photo');
 
 function applyStoredPhoto() {
     document.querySelectorAll('.profile-avatar-photo').forEach((photo) => {
@@ -33,5 +34,21 @@ if (photoInput) {
             }
         });
         reader.readAsDataURL(file);
+    });
+}
+
+if (removePhotoButton) {
+    removePhotoButton.addEventListener('click', () => {
+        localStorage.removeItem('mero-cycle-profile-photo');
+        if (photoPreview) {
+            photoPreview.removeAttribute('src');
+            photoPreview.hidden = true;
+            photoPreview.previousElementSibling.hidden = false;
+        }
+        document.querySelectorAll('.profile-avatar-photo').forEach((photo) => {
+            photo.removeAttribute('src');
+            photo.hidden = true;
+            photo.previousElementSibling.hidden = false;
+        });
     });
 }
